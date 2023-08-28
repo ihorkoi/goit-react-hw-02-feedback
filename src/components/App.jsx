41a5cef6fea1788b522d6023f945fd7d9e0e3ba5
ihorkoi <koleev.igor@gmail.com>
2023-08-28 13:45:12 +0300

    add basic statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Statistics } from './Statistics';
 export const App = () => {
   return (
     <div
@@ -7,10 +8,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Statistics></Statistics>
     </div>
   );
 };
